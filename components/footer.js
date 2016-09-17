87adbdbb9fed586ad5acf51e859df6ce920b7afe
minebreaker_tf <raptors.f22a.blackwidow.yf23@gmail.com>
2016-09-17 12:28:08 +0900

    よく考えたらコンポーネントはいらなかった

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../typings/index.d.ts" />
-"use strict";
-var React = require('react');
-var omniscient = require('omniscient');
-var Footer = omniscient(function () {
-    return React.createElement("div", {className: "navbar navbar-default navbar-fixed-bottom", role: "navigation"}, React.createElement("div", {className: "container"}, React.createElement("p", {className: "navbar-text"}, "Aoba 0.1.0 alpha"), React.createElement("p", {className: "navbar-text navbar-right"}, "Made with ", React.createElement("span", {className: "glyphicon glyphicon-heart", "aria-hidden": "true"}), " by", React.createElement("a", {className: "navlink", href: "https://bitbucket.org/minebreaker_tf/aoba", target: "blank"}, " Aoba"))));
-});
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Footer;
